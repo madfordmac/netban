7f2ddd0ead719e88aa3bcf5013215b98c2eb70cd
Alex Ford <alex@ford-web.net>
2021-04-05 23:41:02 -0500

    Implement the convenience getters in NetBanConfig class.

--- netban/__init__.py
+++ netban/__init__.py
@@ -6,33 +6,33 @@ import asyncio
 
 class NetBanConfig(object):
 	"""Loads the config from file"""
-	def __init__(self, config_file):
+	def __init__(self, config_file_name):
 		super(NetBanConfig, self).__init__()
 		self.__logger = logging.getLogger('netban.config')
 		cfg = configparser.ConfigParser()
-		cfg.read(config_file)
-		self.__logger.info("Read and parsed config file <%s>." % config_file)
+		cfg.read(config_file_name)
+		self.__logger.info("Read and parsed config file <%s>." % config_file_name)
 		self.cfg = cfg
 
 	def get_local_file(self):
 		"""Convenience method for getting the local file to watch."""
-		raise NotImplementedError("Not yet finished.")
+		self.cfg['local'].get('file')
 
 	def get_redis_db(self):
 		"""Convenience method for getting the redis db to use."""
-		raise NotImplementedError("Not yet finised.")
+		self.cfg['local'].getint('db')
 
 	def get_ip_timeout(self):
 		"""Convenience method for getting the timeout period for a single ip."""
-		raise NotImplementedError("Not yet finished.")
+		self.cfg['local'].getint('timeout')
 
 	def get_ip_limit(self):
 		"""Convenience method for getting the max failed attempts for a single ip."""
-		raise NotImplementedError("Not yet finished.")
+		self.cfg['local'].getint('limit')
 
 	def get_rule_number(self):
 		"""Convenience function for getting the location to insert rules in iptables."""
-		raise NotImplementedError("Not yet finished.")
+		self.cfg['general'].getint('rulenum')
 
 class NetBanManager(object):
 	"""Interface with iptables/ipset to manage bans"""
