2079ff817036a805a9185a3519fa789ec5a3ef65
Alex Ford <alex@ford-web.net>
2021-04-21 01:39:36 +0000

    NetBanNet module initial commit.

---
+++ netban/net/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from elasticsearch6 import Elasticsearch
+
+class NetBanNet(object):
+	"""Use logs aggregated in an ELK stack to ban nets with poor policing."""
+	pass
