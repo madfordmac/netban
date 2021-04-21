3d56db022a1feedb1df8d64b8b9c9817b0ee5b98
Alex Ford <alex@ford-web.net>
2021-04-20 23:34:18 -0500

    Progress on NetBanNet. Switching to aiohttp for querying Elasticsearch because async support wasn't added to the specific Elasticsearch modules until the v7 modules.

--- netban/net/__init__.py
+++ netban/net/__init__.py
@@ -1,7 +1,67 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from elasticsearch6 import Elasticsearch
+import aiohttp
+import asyncio
+import logging
 
 class NetBanNet(object):
 	"""Use logs aggregated in an ELK stack to ban nets with poor policing."""
-	pass
+	
+	# This is the json structure for the Elasticsearch query
+	AS_QUERY = {
+		"size": 0,
+		"query": {
+			"bool": {
+				"filter": [
+					{
+						"term": {
+							"ssh_action": "Failed"
+						}
+					},
+					{
+						"range": {
+							"@timestamp": {
+								"gte": "now-7d",
+								"lte": "now"
+							}
+						}
+					}
+				]
+			}
+		},
+		"aggs": {
+			"as": {
+				"terms": {
+					"field": "asn.asn",
+					"size": 10
+				},
+				"aggs": {
+					"n_ips": {
+						"cardinality": {
+							"field": "asn.ip"
+						}
+					}
+				}
+			}
+		}
+	}
+
+	def __init__(self, cfg, ban_manager):
+		super(NetBanNet, self).__init__()
+		self.__logger = logging.getLogger('netban.net')
+		self.cfg = cfg
+		self.ban_manager = ban_manager
+		self.ban_set = set()
+
+	@classmethod
+	async def create(cls, cfg, ban_manager):
+		"""Factory method to create a running instance, since we need to await things."""
+		n = NetBanNet(cfg, ban_manager)
+		await n.updateBanList()
+		return n
+
+	async def updateBanList(self):
+		"""Pull new list of banned nets and update the set."""
+		async with aiohttp.ClientSession() as session:
+			async with session.post(self.query_url, json=self.AS_QUERY) as result:
+				pass
